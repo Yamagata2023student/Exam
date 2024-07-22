a5f2ac65314caa0369fe1fd2cd4a8ebbbf806a1f
soutasat <ymg2365074@stu.o-hara.ac.jp>
2024-07-22 11:00:23 +0900

    beanのsubject

---
+++ src/bean/Subject.java
@@ -0,0 +1,45 @@
+package bean;
+
+import java.io.Serializable;
+
+public class Subject implements Serializable {
+	/**
+	 * 科目コード:String
+	 */
+	private String cd;
+
+	/**
+	 * 科目名:String
+	 */
+	private String name;
+
+	/**
+	 * 所属校:School
+	 */
+	private School school;
+
+
+	public String getCd() {
+		return cd;
+	}
+
+	public void setCd(String cd) {
+		this.cd = cd;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public School getSchool() {
+		return school;
+	}
+
+	public void setSchool(School school) {
+		this.school = school;
+	}
+}
